17f3ddfab7c1ec81524f8b20252f97fdab1e5568
Ebna_Sina <isina413.is@gmail.com>
2022-12-29 15:17:51 +0600

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Ebna Sina**  
+# Jr Software Engineer
+# 
+
+# _I am interested in data science because I feel passonate about data and how we can extract meaningful solutions from data._
+
+# In[1]:
+
+
+a = 2
+b = 3
+print(a+b)
+
+
+# [Google](http://www.google.com) 
+# 
+# <ol>
+# <li>Fish</li>
+# <li>Eggs</li>
+# <li>Cheese</li>
+# </ol>
+# 
+# ~~This is Scratched~~
+
+# In[ ]:
+
+
+
+
